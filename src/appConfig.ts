c1318a99338f409f603b536f529a107df70f98c1
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-03-13 04:52:04 +0800

    update appConfig

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,12 +1,12 @@
 export const app: { [key: string]: string } = {
   name: 'Collabeat', // web app name
   title: 'Collabeat', // website title
-  description: 'Collaborative NFT', // description of website
-  keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
-  url: '', // website url
+  description: 'Audio NFT platform where you can collaborate to create audio memes', // description of website
+  keywords: 'web3, dapp, ethereum, nft, music', // keywords for SEO
+  url: 'collabeat.io', // website url
   image: '/icon-512x512.png', // image url for SEO
   favicon: '/favicon.ico', // favicon url
-  twitter: '@arisdotac', // twitter account
+  twitter: '@collabeat', // twitter account
   themeColor: '#FFFFFF', // theme color
   themeColorDark: '#000000', // theme color dark
 }
